5b6424df5936b1b250520c536b5c97bd295266ad
rubidhexcy <dhexcyrubi@gmail.com>
2025-08-12 22:15:10 -0700

    Add Part 2: Error handling in Future

---
+++ lib/part2_error_handling.dart
@@ -0,0 +1,17 @@
+// File: part2_future_error_handling.dart
+
+Future<String> fetchData() {
+  return Future.delayed(Duration(seconds: 2), () {
+    throw Exception('Something went wrong!');
+  });
+}
+
+void main() async {
+  try {
+    String message = await fetchData();
+    print('Result: $message');
+  } catch (e) {
+    print('Caught an error: $e');
+  }
+  print('Still running even if something goes wrong.');
+}
